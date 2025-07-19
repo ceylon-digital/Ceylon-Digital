0f47b6e12254fb5f36e7b4056319324e5bddd970
Ceylon Digital <madhushanhemachandra1@gmail.com>
2025-07-19 12:39:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,13 +98,13 @@ gsap.timeline()
    .from(".tagline", { y: 30, opacity: 0, duration: 0.6, ease: "power2.out" })
    .from(".headline", { y: 30, opacity: 0, duration: 0.6, ease: "power2.out" })
    .from(".subheadline", { y: 20, opacity: 0, duration: 0.4, ease: "power2.out" }, "-=0.3")
-   /*.from(".cta-buttons a", {
+   .from(".cta-buttons a", {
       opacity: 0,
       y: 20,
       stagger: 0.2,
       duration: 0.4,
       ease: "back.out(1.4)"
-   }, "-=0.3")*/
+   }, "-=0.3")
    .from(".illustration", { x: 100, opacity: 0, duration: 0.7, ease: "power2.out" }, "-=0.4");
 
 
